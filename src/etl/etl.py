a8673285c042873365029c450c0c10cc15e897cf
Uniaut <kunwoo0927@gmail.com>
2022-08-08 18:55:22 +0900

    etl var edit

--- src/etl/etl.py
+++ src/etl/etl.py
@@ -12,7 +12,7 @@ import src.config as config
 def main():
     # extract
     url = config.CHANNEL_URL
-    dl.filter_dl(url, 40)
+    dl.filter_dl(url, 70)
 
     # glob videos
     videos = glob.glob(os.path.join(config.VIDEOS_PATH, '*.mp4'))
