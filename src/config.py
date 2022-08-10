29fda3b7fe2a33ea3aaf14b93a5c1ed91bc2af04
Uniaut <kunwoo0927@gmail.com>
2022-08-11 02:08:00 +0900

    tests

--- src/config.py
+++ src/config.py
@@ -5,8 +5,9 @@ CHANNEL_URL = 'https://www.youtube.com/c/bwftv/videos'
 PLAYLIST_URL = 'https://www.youtube.com/watch?v=teUN-6Us8Uw&list=PLA7ZcagI0frAvDm00pnfYC8hrSlfOXs_Z'
 
 VIDEO_FPS = 30
-VIDEOS_PATH = os.path.join('videos', 'videos')
-VIDEOS_ARCHIVE_PATH = os.path.join('videos', 'list.txt')
+VIDEOS_ROOT = os.path.join('R:', 'Datasets', 'BadmintonDL')
+VIDEOS_PATH = os.path.join(VIDEOS_ROOT, 'videos')
+VIDEOS_ARCHIVE_PATH = os.path.join(VIDEOS_ROOT, 'videos_list.txt')
 MINIMUM_DURATION = 3 * 3600
 
 CONDITION_IMAGE_FILE_PATH = os.path.join('src', 'etl', 'transform', 'feature_extract', 'images')
