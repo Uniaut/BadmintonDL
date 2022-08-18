1ee005f8511941b87cac1601754a2c940eb8d511
Uniaut <kunwoo0927@gmail.com>
2022-08-19 04:25:29 +0900

    dataloader

---
+++ src/winner_pd/dataloader.py
@@ -0,0 +1,53 @@
+import csv
+import glob
+import os
+
+import numpy as np
+import torch
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision.transforms import (
+    Compose,
+    Normalize,
+    RandomCrop,
+    Resize,
+    ToTensor,
+)
+from torchvision.transforms import functional as TF
+
+import src.config as config
+
+
+# Dataloader of labels:
+class BadmintonDataset(Dataset):
+    def __init__(self, data_path, transform=None):
+        self.data_path = data_path
+        self.transform = transform
+        self.labels = self.load_labels()
+
+    def load_labels(self):
+        labels = []
+        play_tuple = []
+        with open(self.data_path, 'r') as tsvfile:
+            readCSV = csv.reader(tsvfile, delimiter='\t')
+            for row in readCSV:
+                frame_n, value = row
+                print(frame_n, value)
+        return labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        image_path = label[0]
+        image = Image.open(image_path)
+        if self.transform:
+            image = self.transform(image)
+        return image, label[1]
+
+if __name__ == '__main__':
+    labels = glob.glob(os.path.join(config.LABELS_PATH, '*.tsv'))
+    for label_path in labels:
+        dl = BadmintonDataset(data_path=label_path)
